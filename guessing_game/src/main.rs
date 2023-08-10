ee4fda0ac669b0b016ebd5d0a25850266d1761ab
tejin3 <tejin3@naver.com>
2023-08-10 10:04:05 +0900

    basic1

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,14 +5,14 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
     // println!("The secret number is: {secret_number}");
 
     loop {
         println!("Please input your guess.");
 
-        let mut guess = String::new();
+        let mut guess: String = String::new();
 
         io::stdin()
             .read_line(&mut guess)
