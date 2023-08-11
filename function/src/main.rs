31f3860b82c68fd83081aec09c4b2f7dd64d1dfd
tejin3 <tejin3@naver.com>
2023-08-11 17:17:29 +0900

    learn

--- function/src/main.rs
+++ function/src/main.rs
@@ -1,9 +1,34 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function();
+    another_function(5, 'h');
+
+    // let x = (let y = 6);
+    let x = five();
+
+    println!("The value of x is: {x}");
+
+    let x = plus_one(5);
+
+    println!("The value of x is: {x}");
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+
+    let y = {
+        let x = 3;
+        x + 1 // ;
+    };
+
+    println!("The value of y is: {y}");
+}
+
+fn five() -> i32 {
+    // 5   ;없음
+    return 5;
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1 // ;
 }
